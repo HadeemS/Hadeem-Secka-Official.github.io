3cc2b9616535290016df30c26d1fc58eec335831
HadeemS <156003202+HadeemS@users.noreply.github.com>
2025-10-26 21:45:24 -0400

    react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+﻿import { Routes, Route } from "react-router-dom";
+
+function Home(){ return <h1 style={{padding:20}}>Home works</h1>; }
+
+export default function App(){
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
