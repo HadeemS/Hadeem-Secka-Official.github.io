4bf48d3ad1acff2cdb8aa7dea0e1479ac14abc03
HadeemS <hadeemsecka@gmail.com>
2025-10-31 18:25:26 -0400

    Fix GitHub Pages blank page: set correct base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Try root first - if repo name matches username.github.io pattern, it may deploy to root
-  // If not working, change to "/Hadeem-Secka-Official.github.io/"
-  base: "/"
+  // Repo is "Hadeem-Secka-Official.github.io" so it deploys to subpath
+  base: "/Hadeem-Secka-Official.github.io/"
 });
